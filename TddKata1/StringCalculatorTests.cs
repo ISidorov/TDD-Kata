ca5263edfebcbe07cb9b4125a9a4dbd5a9e5a917
Igor Sidorov <isidorov@softrust.ru>
2012-01-14 20:40:16 +0300

    TDD Kata 1 Step 1

---
+++ TddKata1/StringCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace TddKata1
+{
+  
+  [TestFixture]
+  public class StringCalculatorTests
+  {
+    private StringCalculator stringCalculator;
+
+    [SetUp]
+    public void Init()
+    {
+      stringCalculator = new StringCalculator();
+    }
+
+    [Test]
+    public void AddEmptyStringWillReturn0()
+    {
+      Assert.AreEqual(0,  stringCalculator.Add(string.Empty));
+    }
+
+    [Test]
+    public void AddOneNumberWillReturnThatNumber()
+    {      
+      Assert.AreEqual(1 , stringCalculator.Add("1"));
+    }
+
+    [Test]
+    public void AddTwoNumbersWillReturnSum()
+    {
+      Assert.AreEqual(3, stringCalculator.Add("1,2"));
+    }
+  }
+
+  public class StringCalculator
+  {
+    public int Add(string numbers)
+    {
+      if (numbers.Equals(string.Empty))
+      {
+        return 0;
+      }
+      var nums = numbers.Split(',');
+      return nums.Sum(num => int.Parse(num));
+    }
+  }
+}
